7251ac51879781fbd874ebe9346af4cc6c66ce1a
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-08-03 15:53:05 -0500

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../Nav/Nav.jsx';
+
+
+const Header = () => (
+  <header className="headerContainer">
+    <Nav />
+  </header>
+);
+
+export default Header;
